e446e47a23dd34888823bc097361d935f65886af
Asiri Dissanayaka <16593060+asiri92@users.noreply.github.com>
2025-01-28 22:04:51 -0800

    Delegate both the registering and creation of the colleagues to the mediator.

--- Program.cs
+++ Program.cs
@@ -7,17 +7,24 @@ namespace Canonical_Mediator
         static void Main(string[] args)
         {
             var mediator = new ConcreateMediator();
+
+            //basic implementation with constructor injection
             //var c1 = new Colleague1(mediator);
             //var c2 = new Colleague2(mediator);
 
-            var c1 = new Colleague1();
-            var c2 = new Colleague2();
+            //var c1 = new Colleague1();
+            //var c2 = new Colleague2();
 
             //mediator.Colleague1 = c1;
             //mediator.Colleague2 = c2;
 
-            mediator.Register(c1);
-            mediator.Register(c2); ;
+            //Use of a method instead of the constructor to setup the bidirectional references
+            //mediator.Register(c1);
+            //mediator.Register(c2); ;
+
+            //Delegate both the registering and creation of the colleagues to the mediator
+            var c1 = mediator.CreateColleague<Colleague1>();
+            var c2 = mediator.CreateColleague<Colleague2>();
 
             c1.Send("Hello, World! (from c1)");
             c2.Send("hi, there! (from c2)");
